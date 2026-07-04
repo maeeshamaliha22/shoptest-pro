7753c9c121d6176f740486ceae16e76f50fa97ce
Maeesha maliha <maeesha.maliha22@gmail.com>
2026-07-04 18:37:46 +0900

    Refactor auth tests to use loggedInPage fixture

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '../fixtures/auth.fixture';
 import { LoginPage } from "../pages/LoginPage";
 
 test.describe('Authentication', () => {
